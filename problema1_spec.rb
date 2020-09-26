68a2b3bcb35603994b7b674010802150e35c0714
Guilherme Chiquet Linux <guilherme.chiqueti@gmail.com>
2020-09-26 18:27:26 -0300

    changed test file names

--- problema1_spec.rb
+++ problema1_spec.rb
@@ -1,24 +1,21 @@
 require './problema1.rb'
 require 'rspec/autorun'
 
-
-person_1 = [{ ruby: 1, db: 1, agile: 0, pattern: 0 }]
-person_2 = [{ ruby: 5, db: 3, agile: 1, pattern: 2 }]
-person_3 = [{ ruby: 5, db: 4, agile: 3, pattern: 3, server: 2 }]
-
-
 describe SeniorTest do
     before do 
         @a = SeniorTest.new
     end
 
     it "test 1 with expected values" do
+        person_1 = [{ ruby: 1, db: 1, agile: 0, pattern: 0 }]
         expect(@a.rank_candidate(person_1)).to eq(2)
     end
     it "test 2 with expected values" do
+        person_2 = [{ ruby: 5, db: 3, agile: 1, pattern: 2 }]
         expect(@a.rank_candidate(person_2)).to eq(11)
     end
     it "test 3 with expected values" do
+        person_3 = [{ ruby: 5, db: 4, agile: 3, pattern: 3, server: 2 }]
         expect(@a.rank_candidate(person_3)).to eq(17)    
     end
 
--- problema2_spec.rb
+++ problema2_spec.rb
@@ -2,9 +2,6 @@ require './problema2.rb'
 require 'rspec/autorun'
 
 
-
-
-
 describe VowelCounter do
     before do
         @a = VowelCounter.new
@@ -15,22 +12,27 @@ describe VowelCounter do
         expected_output = {"a"=> 4, "e"=> 4, "i"=> 4, "o"=> 4, "u"=> 4}
         expect(@a.count_vowels(text_input)).to eq(expected_output)
     end
+
     it "empty string test" do
         text_input = ""
         expected_output = {"a"=> 0, "e"=> 0, "i"=> 0, "o"=> 0, "u"=> 0}
         expect(@a.count_vowels(text_input)).to eq(expected_output)
     end
+    
     it "consonant string text" do
         text_input = " bcd fgh jklmn pqrst vwxyz BCD FGH JKLMN PQRST VWXYZ"
         expected_output = {"a"=> 0, "e"=> 0, "i"=> 0, "o"=> 0, "u"=> 0} 
         expect(@a.count_vowels(text_input)).to eq(expected_output)
     end
+
     it "Special characters string text" do
         text_input = "!@#$%*$#)(*&¨%><:?/}"
         expected_output = {"a"=> 0, "e"=> 0, "i"=> 0, "o"=> 0, "u"=> 0}
         expect(@a.count_vowels(text_input)).to eq(expected_output)
     end
     
+
+
     it "integer parameter" do
         expect{@a.count_vowels(2)}.to raise_error(ArgumentError)
     end
